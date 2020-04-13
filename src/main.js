7beef61971dd81f47447b713c6c1ac848a18df23
Thiago <thiago.pereira@develcode.com.br>
2020-04-12 23:46:14 -0300

    aula5(babel-presets): add env preset and transform runtime

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
+require('babel-runtime/regenerator')
 require('./main.css')
 require('./index.html')
 
-var a = () => {
-  console.log('hello arrow functions')
+var a = async (args) => {
+  const {a, b} = args
+  await console.log('hello arrow functions', a, b)
+  console.log('done')
+
 }
+
+// a({a: 1, b:2})
